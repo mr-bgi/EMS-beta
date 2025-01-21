f3c06faf0a1674dad197a34b3953ed30aa8e93cb
mr-bgi <tola44356@gmail.com>
2025-01-21 09:04:41 +0700

    change auth to authController

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
+const allpage = require('./routes/web/AllPage');
 require('dotenv').config();
 // * api AUTH
 const apiAuth = require('./routes/api/auth');
@@ -11,13 +12,15 @@ const port = process.env.PORT || 8080;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(fileUpload());
+app.use(fileUpload());  
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 // * Static 
 app.use(allpage);
 // * API auth
+app.use(apiAuth);
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
