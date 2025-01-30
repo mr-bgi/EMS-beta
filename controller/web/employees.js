6bdc3c09c7b7a86124d7281deae0858c56609681
saomsopheak101 <sopheaksoam2002@gmail.com>
2025-01-30 23:48:29 +0700

    list emp

--- controller/web/employees.js
+++ controller/web/employees.js
@@ -4,10 +4,14 @@ const con = require('../../config/db')
 const creatEmployee = (req, res) => {
     res.render("page/employee/create-employee");
 };
+const getListEmployee = (req, res) => {
+    res.render("page/employee/list-employee");
+};
 
 
 module.exports = {
     creatEmployee,
+    getListEmployee,
    
 
 }
--- routes/web/employee.js
+++ routes/web/employee.js
@@ -4,6 +4,8 @@ const employeeRoute = require('../../controller/web/employees')
 const router = express.Router();
 
 router.get('/create-employee',employeeRoute.creatEmployee);
+router.get('/list-employee',employeeRoute.getListEmployee);
+
 
 
 module.exports = router;
