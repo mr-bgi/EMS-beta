d126c77ca42945ca5b4057ba97cdef01fb002607
mr-bgi <149460647+mr-bgi@users.noreply.github.com>
2025-02-05 18:56:47 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,37 +1,10 @@
-// const mysql = require('mysql');
-
-// var con = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "",
-//     database: "ems1",
-//     port: 3306 || 3308
-// });
-
-// con.connect(function(error){
-//     if(error) throw error;
-//     console.log("connected to sql");
-// })
-
-// exports.query = (sql,value)=>{
-//     return new Promise((resolv,reject)=>{
-//         con.query(sql,value,(err,result)=>{
-//             if(err) reject(err);
-//             resolv(result)
-//         })
-//     })
-// }
-
-
 const mysql = require('mysql');
 
 var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    // password: "mysql1234" || "",
-    password: "",
+    host: "127.0.0.1",
+    user: "ems4",
+    password: "LXis3d4EccfY2eTT",
     database: "ems4",
-    port: 3308
 });
 
 con.connect(function(error){
